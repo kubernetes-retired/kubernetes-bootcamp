384490ea717cff284e11c6f2d1735d6dbe6f5dfe
Catalin Jora <catalin.jora@container-solutions.com>
2016-08-08 13:35:43 +0200

    changes for katacoda

--- code/docker/v1/server.js
+++ code/docker/v1/server.js
@@ -6,13 +6,13 @@ var host;
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("Hello Kubernetes bootcamp! | Hostname: ");
+  response.write("Hello Kubernetes bootcamp! | Running on: ");
   response.write(host);
   response.end(" | v=1\n");
-  console.log("Hostname:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
 }
 var www = http.createServer(handleRequest);
-www.listen(8080,function () {
+www.listen(8081,function () {
     startTime = new Date();;
     host = process.env.HOSTNAME;
     console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
