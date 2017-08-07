2089a896a826859a021a0bc6390d9d40ab706992
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-08-07 14:49:40 -0700

    katacoda: use bootcamp image from gcr.io/google-samples

--- code/katacoda/module3.sh
+++ code/katacoda/module3.sh
@@ -38,4 +38,4 @@ echo 'esac' >> ~/.bin/minikube
 chmod +x ~/.bin/minikube
 echo 'export KUBERNETES_MASTER=http://host01:8080' >> ~/.bashrc
 ~/.bin/minikube start
-kubectl run kubernetes-bootcamp --image=docker.io/jocatalin/kubernetes-bootcamp:v1 --port=8080
+kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080
