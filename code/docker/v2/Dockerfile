9f180372917670229b433b68368c3a039ffa6a67
Ben Hall <ben@benhall.me.uk>
2018-02-17 00:54:19 +0000

    Update code

---
+++ code/docker/v2/Dockerfile
@@ -0,0 +1,4 @@
+FROM node:slim
+EXPOSE 8080
+COPY server.js .
+CMD node server.js
--- code/docker/v2/server.js
+++ code/docker/v2/server.js
@@ -6,14 +6,14 @@ var host;
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("Hello Kubernetes bootcamp! | Hostname: ");
+  response.write("Hello Kubernetes bootcamp! | Running on: ");
   response.write(host);
-  response.end(" | v=1\n");
-  console.log("Hostname:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+  response.end(" | v=2\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
 }
 var www = http.createServer(handleRequest);
 www.listen(8080,function () {
-    startTime = new Date();;
-    host = process.env.HOSTNAME;
-    console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
+  startTime = new Date();;
+  host = process.env.HOSTNAME;
+  console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
 });
