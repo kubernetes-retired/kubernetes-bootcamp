3ba3c6e05131efa4a3278dab3a53e6e144fd9f61
Ben Hall <ben@benhall.me.uk>
2018-02-19 10:07:00 +0000

    v1 code was also not correct

--- code/docker/v1/server.js
+++ code/docker/v1/server.js
@@ -12,8 +12,8 @@ var handleRequest = function(request, response) {
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
 }
 var www = http.createServer(handleRequest);
-www.listen(8081,function () {
-    startTime = new Date();;
-    host = process.env.HOSTNAME;
-    console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
+www.listen(8080,function () {
+  startTime = new Date();
+  host = process.env.HOSTNAME;
+  console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
 });
